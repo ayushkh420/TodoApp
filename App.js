3b458309b1c5fce53b73304ce54e6d4d0ae78388
Ayush khandelwal <ayushkhandelwal519@gmail.com>
2022-07-30 17:05:19 +0530

    update

--- App.js
+++ App.js
@@ -5,91 +5,95 @@ import {
   View,
   Image,
   Button,
-  ScrollView,
+  Alert,
   TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
   ImageBackground,
   Dimensions,
   FlatList,
 } from 'react-native';
-import Header from './components/header'
-import TodoItem from './components/todoItem'
-import AddTodo from './components/addTodo'
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
-
-export default function App(){
-  const [todos,SetTodos]= useState([
-    {text:'A CUP OF COFFEE', key:'1'},
-    {text:'WATCHING A MOVIE', key:'2'},
-    {text:'READING A BOOK', key:'3'},
+export default function App() {
+  const [todos, SetTodos] = useState([
+    {text: 'A CUP OF COFFEE', key: '1'},
+    {text: 'WATCHING A MOVIE', key: '2'},
+    {text: 'READING A BOOK', key: '3'},
   ]);
 
-  const pressHandler =(key) => {
-    SetTodos((prevTodos) =>{
-      return prevTodos.filter(todos => todos.key!=key);
-    } )
-  }
-  const submitHandler = (text) => {
-      SetTodos((prevTodos) => {
-        return[
-          {text:text,key:Math.random.toString()},
-          ...prevTodos
-        ];
-  })
-}
+  const pressHandler = key => {
+    SetTodos(prevTodos => {
+      return prevTodos.filter(todos => todos.key != key);
+    });
+  };
+  const submitHandler = text => {
+    if (text.length > 3) {
+      SetTodos(prevTodos => {
+        return [{text: text, key: Math.random.toString()}, ...prevTodos];
+      });
+    } else {
+      Alert.alert('OOPS!','Todos must be over 3 chars long',[
+        {text: 'Understood',onPress:() => console.log('alert closed')}
+      ]);
+    }
+  };
 
+  return (
+    <TouchableWithoutFeedback onPress={() => { 
+      Keyboard.dismiss();
+      console.log('Dismissed Keyboard')
+    }}>
 
-  return(
     <View style={styles.container}>
       <ImageBackground
         source={{
-          uri: 
-'https://images.unsplash.com/photo-1616628188550-808682f3926d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+          uri: 'https://images.unsplash.com/photo-1616628188550-808682f3926d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
         }}
         // resizeMode="stretch"
-        style={styles.img}>
-      </ImageBackground>
-      <Header/>
+        style={styles.img}></ImageBackground>
+      <Header />
       <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler}/>
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
-          data={todos}
-           renderItem={({item})  => {
-            return <TodoItem item={item} pressHandler={pressHandler} />
-           }}
-           /> 
+            data={todos}
+            renderItem={({item}) => {
+              return <TodoItem item={item} pressHandler={pressHandler} />;
+            }}
+            />
         </View>
       </View>
     </View>
-  )
+            </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#ffff',
-    imageBackgroundColor:'coral'
+    backgroundColor: '#ffff',
+    imageBackgroundColor: 'coral',
   },
-  content:{
-    padding:40,
-    backgroundColor:'#ffff',
+  content: {
+    padding: 40,
+    backgroundColor: '#ffff',
     position: 'relative',
     opacity: 0.8,
-
   },
-  list :{
-    backgroundColor:'#ffff',
-
+  list: {
+    backgroundColor: '#ffff',
   },
   img: {
     height: screenHeight,
     width: screenWidth,
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'absolute'
+    position: 'absolute',
   },
-  
-})
+});
